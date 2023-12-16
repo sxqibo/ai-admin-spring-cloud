e64372fe9b8609c5d1238d5d471c83529f0a753a
YangHongwei <18903467858@qq.com>
2023-12-16 11:39:19 +0800

    feat: 创建父项目与子项目

---
+++ cloudalibaba-nacos-9001/src/main/java/com/example/cloudalibabanacos9001/CloudalibabaNacos9001Application.java
@@ -0,0 +1,13 @@
+package com.example.cloudalibabanacos9001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudalibabaNacos9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaNacos9001Application.class, args);
+    }
+
+}
