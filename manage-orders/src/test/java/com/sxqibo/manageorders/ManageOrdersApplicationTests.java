6654a112d54d488d0afaa5d7cb36dd8941d60bdb
YangHongwei <18903467858@qq.com>
2023-12-17 01:07:19 +0800

    feat:增加两个模块儿

---
+++ manage-orders/src/test/java/com/sxqibo/manageorders/ManageOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxqibo.manageorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
