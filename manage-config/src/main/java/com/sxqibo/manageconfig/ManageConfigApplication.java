88485b2aca21af1b42f23955ca28af09bb5057e5
YangHongwei <18903467858@qq.com>
2023-12-18 23:22:49 +0800

    feat: Nacos配置中心搭建,获取nacos配置

---
+++ manage-config/src/main/java/com/sxqibo/manageconfig/ManageConfigApplication.java
@@ -0,0 +1,15 @@
+package com.sxqibo.manageconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ManageConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageConfigApplication.class, args);
+    }
+
+}
---
+++ manage-config/src/test/java/com/sxqibo/manageconfig/ManageConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxqibo.manageconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
